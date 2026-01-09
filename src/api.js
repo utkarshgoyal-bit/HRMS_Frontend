eeb3d5251957836d069e0fad99fb20ce42688d05
Maitrii Infotech <maitrii.infotechsolutions@gmail.com>
2026-01-09 17:53:44 +0530

    Update

---
+++ src/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+// Point to your running Backend
+const API = axios.create({
+  baseURL: 'http://127.0.0.1:9999/api/v1',
+});
+
+// Interceptor: Attach Token & Slug to every request
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem('token');
+  const slug = localStorage.getItem('slug'); 
+
+  if (token) {
+    req.headers.Authorization = \Bearer \\;
+  }
+  
+  if (slug) {
+    req.headers['x-tenant-slug'] = slug;
+  }
+
+  return req;
+});
+
+export default API;
